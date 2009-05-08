e7b43e2c7e8759d0bffbf4d24663e147e0156d01
Ari Lerner <arilerner@mac.com>
2009-05-07 18:13:44 -0700

    Moving new version in

--- lib/dslify.rb
+++
@@ -1,7 +0,0 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
-Dir["#{File.dirname(__FILE__)}/dslify/*.rb"].each {|f| require f }
-
-module Dslify
-end
