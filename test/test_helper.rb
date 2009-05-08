ce9280a4b297128dd09f8dec2b2a26663645ab49
Ari Lerner <arilerner@mac.com>
2009-05-08 00:37:18 -0700

    Fixed unit/test tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
 require 'rubygems'
 require 'test/unit'
 require 'shoulda'
+require "context"
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'dslify'
 
 class Test::Unit::TestCase
